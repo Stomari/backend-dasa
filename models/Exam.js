40bd019d13cf6b647a0a09c5be7e17fdf30c056b
Sergio Festa <sfestatomari@gmail.com>
2019-09-01 17:05:06 -0300

    add basic structure and models

---
+++ models/Exam.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const examSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  type: { type: String, required: true, enum: ['analise clinica', 'imagem'] },
+  status: { type: String, required: true, enum: ['ativo', 'inativo'], default: 'ativo' },
+  activeExams: [{ type: Schema.Types.ObjectId, ref: 'Exams' }],
+}, {
+  timestamps: true,
+});
+
+const Exam = mongoose.model('Exam', examSchema);
+module.exports = Exam;
